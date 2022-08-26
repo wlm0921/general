e3867d56c285b583cfc739b24c0ab632020376fb
Weston Murdock <wlm0921@live.unc.edu>
2022-08-26 12:31:35 -0400

    Wrote some exception_handlers for guess_number

--- Small Tutorial Projects/guess_number.py
+++ Small Tutorial Projects/guess_number.py
@@ -4,17 +4,53 @@ __author__: str = "Weston Murdock, wlm0921@live.unc.edu"
 
 import random
 
+# Write game_over() function
+def game_over(f):
+    pass
+
 def guess():
     print("Let's play a game! I'll think of a random number, and you'll try and guess it!")
-    want_to_play: str = input("Do you want to play? (Y/N) ")
-    counter: int = 0
-    # Want To Play chunk goes here, implement counter
+       
+    # Want To Play?
+    while True:
+        try:
+            want_to_play: str = input("Do you want to play? (Y/N) ")
+        except ValueError:
+            print("I didn't understand that, please either type Y or N.")
+        if not (want_to_play == "Y") or (want_to_play == "N"):
+            print("I didn't understand that, please either type Y or N.")
+        else:
+            break
+    
+    if want_to_play == "N":
+        game_over(guess)
+    
+    # Get x
+    while True:
+        try:
+            x: int = int(input("Great! First, what is the smallest number you'd be fine with me choosing? "))
+        except ValueError:
+            print("I didn't understand that, please give me a number.")
+        else:
+            break
+    
+    # Get y
+    while True:
+        try:
+            y: int = int(input("And what is the largest number you'd be fine with me choosing? "))
+        except ValueError:
+            print("I didn't understand that, please give me a number.")
+        if y < x:
+            print(f"Please give me number larger than {x}.")
+        if x == y:
+            print(f"Ok... the game won't be much fun with those numbers, but whatever.")
+            break
+        else:
+            break
 
-    x: int = int(input("Great! First, what is the largest number you'd be fine with me choosing? "))
-    # Add in fail-safe here for x
-
-    random_number: int = random.randint(1,x)
-    guess_number: int = 0
+    random_number: int = random.randint(x,y)
+    guess_number: int = 0 # Figure out what to initialize this to...
+    
     while random_number != guess_number:
         guess_number = int(input(f"Guess a number between 1 and {x}! Guess 0 to give up! "))
         # Figure out how to handle bad inputs here - in particular, strings
